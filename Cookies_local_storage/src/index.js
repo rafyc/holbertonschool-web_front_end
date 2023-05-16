e8d5fdfa8b7e7a3fdaaf3b3494622ceb2b86c2ce
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-05-16 15:24:16 +0200

    +1

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,12 +1,18 @@
 const setCookies = () => {
   inputName = document.getElementById('firstname').value
   inputEmail = document.getElementById('email').value
-  document.cookie = (`username=${inputName}; email=${inputEmail}`)
+  const date = new Date()
+  date.setDate(date.getDate() + 10)
+  const expires = 'expires=' + date.toUTCString()
+  const userName = `username=${inputName}`
+  const userEmail = `email=${inputEmail}`
+  document.cookie = `${userName}; path=/`
+  document.cookie = `${userEmail}; path=/`
+  document.cookie = ` ${expires}; path=/`
 }
 
 const showCookies = () => {
   const para = document.createElement("p");
   para.innerHTML = (`Cookies:${document.cookie}`);
   document.body.appendChild(para);
-  console.log(document.cookie);
 }
